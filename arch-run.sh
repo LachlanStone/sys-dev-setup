c1742365564a3eaa00e29de4a21dd48c3e6078f5
lachlanstone <lachlan.stone2@gmail.com>
2025-06-25 14:35:22 +1000

    fixing deployments

--- arch-run.sh
+++ arch-run.sh
@@ -20,11 +20,13 @@ debug() {
   fi
 }
 arch-required(){
-  sudo pacman -S --needed base-devel
-  git clone https://aur.archlinux.org/paru.git
-  pushd paru || exit
-  makepkg -si
-  popd || exit
+  if command -v pacman &> /dev/null; then
+    debug "$(set_color green) Starting Arch CLI Install $(set_color)"      
+    runs_dir=$(find "$script_dir"/arch-defaults -mindepth 1 -maxdepth 1 -executable)
+    for components in $runs_dir; do
+        $components
+    done
+  fi
 }
 
 
